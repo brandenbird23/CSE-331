a41e5111ac02ff9dbedb7aa1cbbde14a7004f300
Branden <bbird23@uw.edu>
2023-02-14 17:20:19 -0800

    Finished MarvelPaths.

--- hw-marvel/src/main/java/marvel/MarvelPaths.java
+++ hw-marvel/src/main/java/marvel/MarvelPaths.java
@@ -7,12 +7,60 @@ import java.util.*;
 public class MarvelPaths {
     /* Not an ADT */
 
-
+    /**
+     * This method asks the user for input of the marvel file to be used. It then allows
+     * for the user to input two marvel characters to see if they are linked between comic
+     * books. At the end, it will ask the user if they want to continue and search again
+     * or exit the program. If a path is found, it will display so, if none is found, it
+     * will also display so.
+     * @param args
+     */
     public static void main(String[] args) {
         Graph<String, String> marvelGraph = MarvelPaths.graphCreator("marvel.csv");
         Scanner input = new Scanner(System.in);
+        Scanner answer = new Scanner(System.in);
+        boolean again = true;
+        String char1, char2;
+        while (again) {
+            System.out.println("Here you can find connections of two marvel characters by comic books!");
+            System.out.println("Please type the name of the first character: ");
+            char1 = input.nextLine().toUpperCase();
+            System.out.println("Please type the name of the second character: ");
+            char2 = input.nextLine().toUpperCase();
 
-        //TODO
+            if (!(marvelGraph.containsNode(char1))) {
+                System.out.println(char1 + " is an unknown character");
+            } else if (!(marvelGraph.containsNode(char2))) {
+                System.out.println(char2 + " is an unknown character");
+            } else {
+                System.out.println();
+                System.out.println("Searching for " + char1 + " and " + char2 + " paths");
+                List<Graph<String, String>.Edge> path = BFS(marvelGraph, char1, char2);
+                if (path == null) {
+                    System.out.println("Path from" + char1 + " to " + char2 + " was not found.");
+                } else {
+                    for (Graph<String, String>.Edge edge : path) {
+                        System.out.println(edge.getParent() + " and " + edge.getChild() +
+                                " are in " + edge.getLabel());
+                    }
+                }
+            }
+
+            System.out.println();
+            System.out.println("Would you like to search again? \n" +
+                    "Enter (y) to continue or enter (n) to exit");
+            String decision = answer.nextLine().toLowerCase();
+            if(decision.charAt(0) == 'y') {
+                again = true;
+            } else if (decision.charAt(0) == 'n') {
+                again = false;
+            } else {
+                System.out.println("Please use (y/n)");
+            }
+        }
+        System.out.println("Have a nice day!");
+        input.close();
+        answer.close();
     }
 
     /**
