27c12684c295dcf76e80da564558734be7c28b77
Branden <bbird23@uw.edu>
2023-02-16 19:13:59 -0800

    typo fixed

--- hw-marvel/src/main/java/marvel/MarvelParser.java
+++ hw-marvel/src/main/java/marvel/MarvelParser.java
@@ -43,7 +43,7 @@ public class MarvelParser {
                 comics.put(comic, new ArrayList<>());
             }
 
-            // dno duplicates in the characterList for each comic
+            // no duplicates in the characterList for each comic
             if (!(comics.get(comic).contains(character))) {
                 comics.get(comic).add(character);
             }
