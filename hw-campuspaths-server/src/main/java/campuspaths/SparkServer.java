3036d255b3dd1c28e2819f3d0f2d4684c8b92269
Branden <bbird23@uw.edu>
2023-03-05 18:16:02 -0800

    SparkServer finished I think

--- hw-campuspaths-server/src/main/java/campuspaths/SparkServer.java
+++ hw-campuspaths-server/src/main/java/campuspaths/SparkServer.java
@@ -12,6 +12,16 @@
 package campuspaths;
 
 import campuspaths.utils.CORSFilter;
+import com.google.gson.Gson;
+import pathfinder.CampusMap;
+import pathfinder.datastructures.Path;
+import pathfinder.datastructures.Point;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.util.Map;
 
 public class SparkServer {
 
@@ -23,7 +33,41 @@ public class SparkServer {
         // comes from a different server.
         // You should leave these two lines at the very beginning of main().
 
-        // TODO: Create all the Spark Java routes you need here.
+        CampusMap map = new CampusMap();
+        Spark.get("/route-paths", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                String startBuilding = request.queryParams("start");
+                String endBuilding = request.queryParams("end");
+                Path<Point> shortestPath = null;
+                // half execution to return response to client if start or end is null
+                if (startBuilding == null || endBuilding == null) {
+                    Spark.halt(400, "Must have Start and End");
+                } else if (!(map.shortNameExists(startBuilding))) {
+                    Spark.halt(400, "Start Building must exist");
+                } else if (!(map.shortNameExists(endBuilding))) {
+                    Spark.halt(400, "End Building must exist");
+                } else {
+                    // trys to create the shortest path using Dijkstra's Algorithm
+                    try {
+                        shortestPath = map.findShortestPath(startBuilding, endBuilding);
+                    } catch (IllegalArgumentException e) {
+                        Spark.halt(400, "Unable to find path");
+                    }
+                }
+                Gson gson = new Gson();
+                return gson.toJson(shortestPath);
+            }
+        });
+
+        Spark.get("/buildings", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                Map<String, String> buildings = map.buildingNames();
+                Gson gson = new Gson();
+                return gson.toJson(buildings.keySet());
+            }
+        });
     }
 
 }
